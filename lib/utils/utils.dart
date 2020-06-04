da241b4638317f2d926bcb05d626212c0fbcc0a5
Simon Pham <simonpham.dn@gmail.com>
2020-06-05 01:40:54 +0700

    feat: add async catch all function

--- lib/utils/utils.dart
+++ lib/utils/utils.dart
@@ -1,5 +1,33 @@
-catchAll(Function() function) {
+typedef ErrorCallback(T);
+
+/// Runs the `function` and ignore all the errors.
+///
+/// Runs function [ErrorCallback] `onError` when there is an error
+/// Returns `true` whether there is no error.
+bool catchAll(Function function, {ErrorCallback onError}) {
   try {
     function();
-  } catch (_) {}
+    return true;
+  } catch (error) {
+    if (onError != null) {
+      onError(error);
+    }
+  }
+  return false;
+}
+
+/// Runs the async `function` and ignore all the errors.
+///
+/// Runs function [ErrorCallback] `onError` when there is an error
+/// Returns `true` whether there is no error.
+Future<bool> asyncCatchAll(Function function, {ErrorCallback onError}) async {
+  try {
+    await function();
+    return true;
+  } catch (error) {
+    if (onError != null) {
+      await onError(error);
+    }
+  }
+  return false;
 }
